3f41ee854a876e9e383f52749ecdea360eaf05b2
damfinkel <damfinkel@gmail.com>
2020-04-02 18:53:15 -0300

    angular login

---
+++ angular/tech-show-frontend/src/app/services/auth.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable()
+export class AuthService {
+  constructor(private httpClient: HttpClient) {}
+
+  login(user): Observable<HttpResponse<any>> {
+    return this.httpClient.post<any>(
+      `${environment.baseURL}/users/login`,
+      user,
+      { observe: 'response' }
+    );
+  }
+
+  getToken() {
+    return localStorage.getAuthToken();
+  }
+
+  setToken(token) {
+    localStorage.authToken = token;
+  }
+
+  isLogged(): boolean {
+    return !!this.getToken();
+  }
+}
